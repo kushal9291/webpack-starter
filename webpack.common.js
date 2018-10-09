84eb14342e828f4b7f44b9b6108cf1eb88d374fc
Kushal Parikh <kushal9291@gmail.com>
2018-10-09 16:09:04 +0530

    Html beautifyer added

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlBeautifyPlugin = require('html-beautify-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin'); 
 const webpack = require('webpack'); 
 
@@ -77,6 +78,17 @@ module.exports = {
             title: 'Blank File',
             filename: path.resolve(__dirname, 'dist/blank.html'),
             template: path.resolve(__dirname, 'src/views/blank.html')
+        }),
+         new HtmlBeautifyPlugin({
+            config: {
+                html: {
+                    end_with_newline: true,
+                    indent_size: 1,
+                    indent_with_tabs: true,
+                    indent_inner_html: true 
+                }
+            },
+            replace: [ ' type="text/javascript"' ]
         })
     ]
  
