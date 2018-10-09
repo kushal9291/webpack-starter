0f8ba080a5af168aba9136ab90dd4571c7bd7cd0
Kushal Parikh <kushal9291@gmail.com>
2018-10-09 12:53:17 +0530

    webpack configration source map removed from dist and added only in dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const webpack = require('webpack');
 
 module.exports = merge(common, {
@@ -12,6 +15,20 @@ module.exports = merge(common, {
         port: 9000,
         hot: true
     },
+    optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: true // set to true if you want JS source maps
+          }),
+          new OptimizeCSSAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { sourcemap: true }
+          })
+        ]
+      },
     module: {
         rules: [
             {
